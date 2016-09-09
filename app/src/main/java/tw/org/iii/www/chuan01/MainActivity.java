33b5583ce263c1ff71e89e2ad9e17974541d89de
chuan1983 <chuan1983126@gmail.com>
2016-09-09 13:40:24 +0800

    lattey

--- app/src/main/java/tw/org/iii/www/chuan01/MainActivity.java
+++ app/src/main/java/tw/org/iii/www/chuan01/MainActivity.java
@@ -2,12 +2,29 @@ package tw.org.iii.www.chuan01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnclick;
+    private TextView mesg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnclick = (Button) findViewById(R.id.btnclick);
+        mesg = (TextView) findViewById(R.id.mesg);
+
+        btnclick.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesg.setText("樂透號碼:"+(int)(Math.random()*49));
+            }
+        });
+
     }
 }
